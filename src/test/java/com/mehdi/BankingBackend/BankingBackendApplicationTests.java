e8aa69877556e53d2dc94840b81ffc21d4ae8c28
Mehdi Faiz <mehdifaiz42@outlook.com>
2025-08-16 10:35:23 +0200

    first commit

---
+++ src/test/java/com/mehdi/BankingBackend/BankingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehdi.BankingBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
