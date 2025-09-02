ff5bc008d5fe2c437ec9430f9b1f16d1dcae4a2c
Mehdi-Faiz <mehdifaiz42@outlook.com>
2025-09-02 06:56:14 +0200

    started working on secuirty/auth

--- src/main/java/com/mehdi/BankingBackend/config/SecurityConfig.java
+++ src/main/java/com/mehdi/BankingBackend/config/SecurityConfig.java
@@ -1,21 +1,32 @@
 package com.mehdi.BankingBackend.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())           // disable CSRF for testing
-                .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()           // allow all requests
-                );
+    public InMemoryUserDetailsManager configuration() {
 
-        return http.build();
+        List<UserDetails> users = new ArrayList<>();
+        List<GrantedAuthority> adminAuthority = new ArrayList<>();
+        adminAuthority.add(new SimpleGrantedAuthority("ADMIN"));
+        UserDetails admin = new User("devs", "{noop}devs", adminAuthority);
+        users.add(admin);
+
+        return new InMemoryUserDetailsManager(users);
     }
 }
