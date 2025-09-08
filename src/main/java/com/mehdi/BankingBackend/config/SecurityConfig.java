81cae422d207c6a822f5567627020fedde4ea84c
Mehdi-Faiz <mehdifaiz42@outlook.com>
2025-09-08 18:57:27 +0200

    Basic form of security has been implemented. Apis now require the user to be logged in

--- src/main/java/com/mehdi/BankingBackend/config/SecurityConfig.java
+++ src/main/java/com/mehdi/BankingBackend/config/SecurityConfig.java
@@ -1,32 +1,50 @@
 package com.mehdi.BankingBackend.config;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
+
+public class SecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager configuration() {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**",
+                                "/swagger-resources/**",
+                                "/webjars/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .logout(logout -> logout.permitAll());
 
-        List<UserDetails> users = new ArrayList<>();
-        List<GrantedAuthority> adminAuthority = new ArrayList<>();
-        adminAuthority.add(new SimpleGrantedAuthority("ADMIN"));
-        UserDetails admin = new User("devs", "{noop}devs", adminAuthority);
-        users.add(admin);
-
-        return new InMemoryUserDetailsManager(users);
+        return http.build();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
 }
